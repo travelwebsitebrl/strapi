0d0f3736763f4733e935962c90398f39c4a24640
Shivansh2287 <54775060+Shivansh2287@users.noreply.github.com>
2021-01-06 10:51:33 +0530

    blog

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c7d68f721f6dafdb494b8230a5d71a97'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3d263c1e-6bad-4624-8c1d-1f6e0f8d6fc2'
+};
